6c1e5411c51fd5697aec45cd42718d08a848c0af
Ayush Kumar <ayushkumar08032003@gmail.com>
2022-07-26 16:02:36 +0530

    Birthday Application Done

--- app/src/main/java/com/ayushwalker/birthdayapplication/BirthdayGreetingActivity.kt
+++ app/src/main/java/com/ayushwalker/birthdayapplication/BirthdayGreetingActivity.kt
@@ -2,10 +2,15 @@ package com.ayushwalker.birthdayapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_birthday_greeting.*
 
 class BirthdayGreetingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_birthday_greeting)
+
+        val name = intent.getStringExtra("name1")
+        birthdayGreeting.text = "Happy Birthday\n $name!!"
+
     }
 }
--- app/src/main/java/com/ayushwalker/birthdayapplication/MainActivity.kt
+++ app/src/main/java/com/ayushwalker/birthdayapplication/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.ayushwalker.birthdayapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +15,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun createBirthdayCard(view: View) {
+//        val name = editTextPersonName.editableText.toString()
+//        Toast.makeText(this, "Name is $name" , Toast.LENGTH_SHORT).show()
+
+        val intent = Intent(this, BirthdayGreetingActivity::class.java)
         val name = editTextPersonName.editableText.toString()
-        Toast.makeText(this, "Name is $name" , Toast.LENGTH_SHORT).show()
+        intent.putExtra("name1",name)
+        startActivity(intent)
     }
 }
